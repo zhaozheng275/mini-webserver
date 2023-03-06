32a66254558a50732ce75dd3b2dc750d3fa36357
zhaozheng275 <83907102+zhaozheng275@users.noreply.github.com>
2023-03-06 20:01:34 +0800

    Update main.cpp

--- WebServer/code/main.cpp
+++ WebServer/code/main.cpp
@@ -3,9 +3,6 @@
 
 int main()
 {
-    /* 守护进程 后台运行 */
-    // daemon(1, 0);
-
     WebServer server(
         1316, 3, 60000, false,             /* 端口 ET模式 timeoutMs 优雅退出  */
         3306, "root", "root", "webserver", /* Mysql配置 */
